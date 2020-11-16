b3c5987a13d53b9e303e20562894d474b041746e
陈测试 <1195733026@qq.com>
2020-11-16 14:47:13 +0800

    Update urls.py

--- WebForAutoTestPlatForm/urls.py
+++ WebForAutoTestPlatForm/urls.py
@@ -1,7 +1,7 @@
-"""WebForAutoTestPlatForm URL Configuration
+"""WebforAutoTestPlatform URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,10 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+
 from django.urls import path
+
+from Report_Platform import report_views
 import xadmin
+from Test_Platform import test_views
+
+
 urlpatterns = [
-    # path('admin/', admin.site.urls),
     path('xadmin/', xadmin.site.urls),
+    path('xadmin/mobile_test_view/suite_test/', test_views.suite_exe, name='suite_exe'),
+    # path('xadmin/web_report_view/web_report/', report_views.WebReportView, name='web_report'),
 ]
